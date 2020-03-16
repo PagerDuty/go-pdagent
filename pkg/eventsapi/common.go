9e6d64558283cc1146a0e097757c78a293a1f1a6
Daniel O'Brien <dobrien@pagerduty.com>
2020-03-16 15:26:08 -0400

    Initial Events API support.
    Decided to add support for both V1 and V2 events, and through a common
    interface even though they'll come through a single channel for the time
    being.
    
    For now I'm just treating enums and timestamps as strings, but might do
    a separate pass to lock them down. We also aren't doing any validation
    at the moment, but could be something to consider in the future.
    
    Will add tests (including mocking) in a subsequent PR.

---
+++ pkg/eventsapi/common.go
@@ -0,0 +1,60 @@
+package eventsapi
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+// UnrecognizedEventType occurs when an event isn't supported by the events
+// API.
+const UnrecognizedEventType = errors.New("unrecognized event type")
+
+// enqueueEvent handles common operations around encoding, sending, then
+// receiving and decoding from both the V1 and V2 events APIs.
+func enqueueEvent(context context.Context, client *http.Client, url string, event interface{}, response interface{}) error {
+	body, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	json.NewDecoder()
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.WithContext(context)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(respBody, &response); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Enqueue an event to either the V1 or V2 events API depending on event type.
+func Enqueue(context context.Context, client *http.Client, event interface{}) (interface{}, error) {
+	switch e := event.(type) {
+	case EventV1:
+		return CreateV1(context, client, e)
+	case EventV2:
+		return EnqueueV2(context, client, e)
+	default:
+		return nil, UnrecognizedEventType
+	}
+}
---
+++ pkg/eventsapi/v2.go
@@ -0,0 +1,63 @@
+package eventsapi
+
+import (
+	"context"
+	"net/http"
+)
+
+const endpointV2 = "https://events.pagerduty.com/v2/enqueue"
+
+// EventV2 corresponds to a V2 event object.
+type EventV2 struct {
+	RoutingKey  string    `json:"routing_key"`
+	EventAction string    `json:"event_action"`
+	DedupKey    string    `json:"dedup_key,omitempty"`
+	Payload     PayloadV2 `json:"payload"`
+	Images      []ImageV2 `json:"images,omitempty"`
+	Links       []LinkV2  `json:"links,omitempty"`
+}
+
+// PayloadV2 corresponds to a V2 payload object.
+type PayloadV2 struct {
+	Summary       string                 `json:"summary"`
+	Source        string                 `json:"source"`
+	Severity      string                 `json:"severity"`
+	Timestamp     string                 `json:"timestamp,omitempty"`
+	Component     string                 `json:"component,omitempty"`
+	Group         string                 `json:"group,omitempty"`
+	Class         string                 `json:"class,omitempty"`
+	CustomDetails map[string]interface{} `json:"custom_details,omitempty"`
+}
+
+// ImageV2 corresponds to a V2 image object.
+type ImageV2 struct {
+	Source string `json:"src"`
+	Href   string `json:"href,omitempty"`
+	Alt    string `json:"alt,omitempty"`
+}
+
+// LinkV2 corresponds to a V2 link object.
+type LinkV2 struct {
+	Href string `json:"href"`
+	Text string `json:"text"`
+}
+
+// ResponseV2 corresponds to a V2 response object.
+type ResponseV2 struct {
+	Status   string   `json:"status,omitempty"`
+	Message  string   `json:"message,omitempty"`
+	DedupKey string   `json:"dedupkey,omitempty"`
+	Errors   []string `json:"errors,omitempty"`
+}
+
+// EnqueueV2 sends an event explicitly to the Events API V2.
+func EnqueueV2(context context.Context, client *http.Client, event EventV2) (*ResponseV2, error) {
+	response := new(ResponseV2)
+
+	err := enqueueEvent(context, client, endpointV2, event, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
