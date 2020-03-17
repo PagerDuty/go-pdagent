d4e8c62c5ecdaf82cb5d89c2d6477d6d6db80c4b
Ashley Brooks <ashley.brooks.a@gmail.com>
2020-03-17 15:16:37 -0700

    Add cobra commands with flags (#2)
    * Add cobra and commands
    
    * Add changes to cobra cmds and modify license
    
    * Add flags and cmd descriptions for queue and send commands
    
    * Fix bug

---
+++ cmd/queue.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2020 PagerDuty, Inc. <info@pagerduty.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// queueCmd represents the queue command
+var queueCmd = &cobra.Command{
+	Use:   "queue",
+	Short: "Access local queue of PagerDuty Agent",
+	Long: `TODO: A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("queue called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(queueCmd)
+
+	queueCmd.PersistentFlags().StringP("routing-key", "k", "", "Service Events API routing key")
+}
---
+++ cmd/send.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2020 PagerDuty, Inc. <info@pagerduty.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// sendCmd represents the send command
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "Queue up a trigger, acknowledge, or resolve event to PagerDuty",
+	Long: `TODO: A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("send called", cmd.Flag("routing-key").Value)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+
+	sendCmd.PersistentFlags().StringP("routing-key", "k", "", "Service Events API Key")
+	sendCmd.PersistentFlags().StringP("event-type", "t", "", "Event type")
+	sendCmd.PersistentFlags().StringP("description", "d", "", "Short description of the problem")
+	sendCmd.PersistentFlags().StringP("incident-key", "i", "", "Incident Key")
+	sendCmd.PersistentFlags().StringP("client", "c", "", "Client")
+	sendCmd.PersistentFlags().StringP("client-url", "u", "", "Client URL")
+	sendCmd.PersistentFlags().StringP("field", "f", "", "Add given KEY=VALUE pair to the event details")
+}
