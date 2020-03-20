5ddd146d28b8e428c3708fc22f1e57de9a52802d
Ashley Brooks <ashley.brooks.a@gmail.com>
2020-03-20 14:43:43 -0700

    Add server file and connect it to cobra server command

---
+++ pkg/server.go
@@ -0,0 +1,14 @@
+package pkg
+
+import "net/http"
+
+func Start() {
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+}
