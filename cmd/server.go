5cf1368eb4426b70fac53ce7693025a8d07cc873
Lewis Rafuse <lrafuse@pagerduty.com>
2021-07-06 00:17:19 -0400

    Remove old unecessary default.

--- cmd/server.go
+++ cmd/server.go
@@ -42,7 +42,7 @@ func NewServerCmd() *cobra.Command {
 
 	defaults := cmdutil.GetDefaults()
 
-	cmd.PersistentFlags().String("database", defaults.Database, "database file for event queuing (default is /var/db/pdagent/agent.db)")
+	cmd.PersistentFlags().String("database", defaults.Database, "database file for event queuing")
 	cmd.PersistentFlags().String("region", defaults.Region, `PagerDuty region the daemon sends events to, either "us" or "eu"`)
 
 	if err := viper.BindPFlag("database", cmd.PersistentFlags().Lookup("database")); err != nil {
